bd24edfc854adef5da065bef8d693f4783444dc2
Jose Martin Escobar Robles <martinflowjose@gmail.com>
2025-11-08 14:58:36 -0600

    Remove API key from production environment
    Removed the API key for security reasons.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  apiKey: 'ddc6f9901d4211d821537479c5b2a5e0',
+  apiKey: '',
   imgPath: 'https://image.tmdb.org/t/p'
 };
